8d96c022f4a7e93a8d2586985cf0d0fa5e2407e3
ANISHA_PATTANAYAK_MCA_2024_26_06_UEMK <pattanayakanisha2003@gmail.com>
2025-01-19 14:17:24 +0530

    Update 10num.py

--- 10num.py
+++ 10num.py
@@ -1,7 +1,3 @@
-public class PrintTenNumbers {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-    }
-}
+for i in range(1, 11):
+    print(i)
+
